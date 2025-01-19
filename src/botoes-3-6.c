56ec9f57ab31d431643ad00773ab1017211545d0
JoaogSacerdote <joaog.souza2002@gmail.com>
2025-01-19 16:08:45 -0300

    Adicionada função "tocar_nota"
    Controla a frequência e duração de sons no buzzer.

--- src/botoes-3-6.c
+++ src/botoes-3-6.c
@@ -2,3 +2,9 @@
 #include "pico/stdlib.h"
 #include "buzzer.h"
 #include "leds.h"
+
+void tocar_nota(uint pino_buzzer, int frequencia, int duracao) {
+    controlar_buzzer(pino_buzzer, true);
+    sleep_ms(duracao);
+    controlar_buzzer(pino_buzzer, false);
+}
