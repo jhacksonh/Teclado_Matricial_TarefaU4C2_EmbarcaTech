de3224bb92a5690824a069334ef2ff61e2931294
JoaogSacerdote <joaog.souza2002@gmail.com>
2025-01-19 16:09:58 -0300

    Adicionada função "leds_ritmo"
    primeiro padrão básico para LEDs.

--- src/botoes-3-6.c
+++ src/botoes-3-6.c
@@ -8,3 +8,13 @@ void tocar_nota(uint pino_buzzer, int frequencia, int duracao) {
     sleep_ms(duracao);
     controlar_buzzer(pino_buzzer, false);
 }
+
+void leds_ritmo(int ritmo) {
+    if (ritmo == 1) {
+        controlar_leds(13, true, 12, false, 11, false);  // LED vermelho
+        sleep_ms(300);
+        controlar_leds(13, false, 12, false, 11, false);
+        sleep_ms(300);
+    }
+    // Adicionar outros padrões de LED conforme necessário
+}
